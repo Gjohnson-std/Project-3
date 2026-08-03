866bfefe3fec9ea92b7d6d5d4f3cb0680ac44b41
Gjohnson-std <gabriellejohnson508@icloud.com>
2026-08-02 20:56:26 -0400

    print menu screen template

--- printmenu.java
+++ printmenu.java
@@ -1,59 +1,4 @@
 
-import java.io.*;
-import java.util.Scanner;
-import java.sql.*;
-
-public class Driver {
-
-    public static void main(String[] args) throws SQLException, IDException
-    {
-        //connecting to database server (oracle)
-        try 
-        {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-    
-        }
-        catch(ClassNotFoundException e)
-        {
-            System.out.println("Could not load the driver");
-        }
-        //connect to oracle and validate the username and password 
-        try 
-        {
-            String Oracleuser,Oraclepass;
-            String url="jdbc.oracle:thin;@10.251.216.48:1521:xe";
-            Connection conn = DriverManager.getConnection(url,"c##testm","t123456");
-
-            //print menu
-            Scanner scan = new Scanner(System.in);
-            SQLCommands SQL = new SQLCOMMANDS();
-            int command;
-            String input;
-            boolean keepGoing = tru;
-            while (keepGoing == true)
-            {
-                printmenu();
-                input = scan.nextLine();
-                command = Integar.parseInt(input);
-
-                switch(command)
-                {
-                    case 1: SQL.add_course(conn, scan): break;
-                    case 2: SQL.show_courses(conn); break;
-                    case 0: 
-                        System.out.println("The session has ended, Thank you!");
-                        keepGoing = false;
-                        conn.close(); 
-                        break;
-                }
-            }
-        } 
-        catch (SQLException ex)
-        {
-        System.out.println("An error occured when connecting to the database server.");
-        ex.printStakcTrace();
-        }
-    }
     //making the start screen 
     public static void printmenu()
     {
@@ -73,7 +18,3 @@ public class Driver {
         System.out.println(" 8. Quit");
 
     }
-
-    //scanner used to gather user input 
-
-}
